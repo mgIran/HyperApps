9864fb8f61adcab5be8e51ccbf41ffb7adb2ddd0
Yusef Mobasheri <yusef.mobasheri@gmail.com>
2016-05-04 15:12:04 +0430

    Fix Bugs

--- protected/views/apps/buy.php
+++ protected/views/apps/buy.php
@@ -5,7 +5,7 @@
 /* @var $bought boolean */
 ?>
 
-<div class="col-md-6 col-md-offset-3">
+<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 buy-box">
     <div class="panel panel-default">
         <div class="panel-heading">
             <h3 class="panel-title">خرید</h3>
